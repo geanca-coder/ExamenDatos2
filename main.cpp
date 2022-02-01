e7a54a7cfa6e534d59e7c5f230d34565cf63d92f
Jose Geancarlo Bermudez Blandon <geancarlosberm54@gmail.com>
2022-02-01 12:31:04 -0600

    Buscando hacer el conflicto

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+using namespace std;
 int main(){
-
+    for(int i =0; i<10; i++){
+        //haciendo una caracteristica de merge
+        cout<<"Caracteristica"<<endl;
+    }
     return 0;
 }
