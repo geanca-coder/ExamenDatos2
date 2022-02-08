19dad2514d2b9a7d5ffe023b22610c741a922864
Jose Geancarlo Bermudez Blandon <geancarlosberm54@gmail.com>
2022-02-08 09:57:04 -0600

    proyecto actualizado con la carga correspondiente

---
+++ FileManager.cpp
@@ -0,0 +1,31 @@
+//
+// Created by geanc on 8 feb. 2022.
+//
+
+#include "FileManager.h"
+
+Graph<string> *FileManager::graphMaker(Graph<string> *graph) {
+    graph = new Graph<string>;
+    fstream file;
+    char lim = ',';
+    string node1, node2, weight;
+    file.open("../DATABASE/EXAM2.csv", ios::in);
+    if(file.is_open()){
+        while(!file.eof()) {
+            getline(file, node1, lim);
+            getline(file, node2, lim);
+            getline(file, weight, '\n');
+            graph->addEdge(node1,node2, stoi(weight));
+
+        }
+    }
+    return graph;
+}
+
+FileManager::FileManager() {
+
+}
+
+FileManager::~FileManager() {
+
+}
---
+++ FileManager.h
@@ -0,0 +1,19 @@
+//
+// Created by geanc on 8 feb. 2022.
+//
+
+#ifndef EXAMENDATOS2_FILEMANAGER_H
+#define EXAMENDATOS2_FILEMANAGER_H
+
+#include "Graph.h"
+#include <fstream>
+class FileManager {
+public:
+    FileManager();
+    Graph<string>* graphMaker(Graph<string>*graph);
+    ~FileManager();
+
+};
+
+
+#endif //EXAMENDATOS2_FILEMANAGER_H
